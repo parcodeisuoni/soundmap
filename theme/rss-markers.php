f55673751858bd101987b2956526ed036b985f5a
parcodeisuoni <parcodeisuoni@gmail.com>
2015-06-15 13:15:43 +0200

    Code reformat - theme folder
    - improving code readability

--- theme/rss-markers.php
+++ theme/rss-markers.php
@@ -4,10 +4,10 @@
  *
  * @package WordPress
  */
-header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
+header( 'Content-Type: ' . feed_content_type( 'rss-http' ) . '; charset=' . get_option( 'blog_charset' ), true);
 $more = 1;
 
-echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
+echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>'; ?>
 
 <rss version="2.0"
 	xmlns:dc="http://purl.org/dc/elements/1.1/"
@@ -24,36 +24,36 @@ $args = array(
         'orderby' => 'date'
 );
 query_posts( $args );
-$lang = get_option('rss_language');
+$lang = get_option( 'rss_language' );
 
 ?>
-<channel xml:lang="<?php echo $lang;?>">
-	<title><?php bloginfo_rss('name');?></title>
+<channel xml:lang="<?php echo $lang; ?>">
+	<title><?php bloginfo_rss( 'name' ); ?></title>
 	<atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml"/>
-	<link><?php bloginfo_rss('url') ?></link>
-	<description><?php bloginfo_rss("description") ?></description>
+	<link><?php bloginfo_rss( 'url' ); ?></link>
+	<description><?php bloginfo_rss( "description" ); ?></description>
 	<language><?php echo $lang; ?></language>
 	<itunes:image href="<?php echo WP_PLUGIN_URL ?>/soundmap/img/soinumapa.jpg" />
 	<image>
-		<title><?php bloginfo_rss('name');?></title>
+		<title><?php bloginfo_rss( 'name' ); ?></title>
 		<url>
-			<?php echo WP_PLUGIN_URL ?>/soundmap/img/soinumapa_small.jpg
+			<?php echo WP_PLUGIN_URL . '/soundmap/img/soinumapa_small.jpg' ?>
 		</url>
 		<link>http://www.soinumapa.net</link>
 		<width>140</width>
 		<height>140</height>
 	</image>
-	<?php do_action('rss2_head'); ?>
-	<?php while( have_posts()) : the_post(); ?>
+	<?php do_action( 'rss2_head' ); ?>
+	<?php while( have_posts() ) : the_post(); ?>
 	<item xml:lang="<?php echo $lang; ?>">
-		<title><?php the_title_rss() ?></title>
-		<link><?php the_permalink_rss() ?></link>
-		<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_post_time('Y-m-d H:i:s', true), false); ?></pubDate>
+		<title><?php the_title_rss(); ?></title>
+		<link><?php the_permalink_rss(); ?></link>
+		<pubDate><?php echo mysql2date( 'D, d M Y H:i:s +0000', get_post_time( 'Y-m-d H:i:s', true ), false ); ?></pubDate>
 
-		<?php the_category_rss('rss2') ?>
+		<?php the_category_rss( 'rss2' ); ?>
 
 		<guid isPermaLink="true"><?php the_guid(); ?></guid>
-<?php if (get_option('rss_use_excerpt')) : ?>
+<?php if ( get_option( 'rss_use_excerpt') ) : ?>
 		<description><![CDATA[<?php the_excerpt_rss() ?>]]></description>
 <?php else : ?>
 		<description><![CDATA[<?php the_excerpt_rss() ?>]]></description>
@@ -65,7 +65,7 @@ $lang = get_option('rss_language');
 <?php endif; ?>
 
 <?php soundmap_rss_enclosure(); ?>
-	<?php do_action('rss2_item'); ?>
+	<?php do_action( 'rss2_item' ); ?>
 	</item>
 	<?php endwhile; ?>
 </channel>
--- theme/theme_window.php
+++ theme/theme_window.php
@@ -3,18 +3,22 @@
 ?>
 
 
-<div <?php post_class('marker-window', $id) ?>>
-    <div class="post-title"><h3><a href="<?php echo get_permalink($id);?>"><?php echo get_the_title($id); ?></a></h3></div>
+<div <?php post_class( 'marker-window', $id ); ?>>
+	<div class="post-title">
+		<h3><a href="<?php echo get_permalink( $id ); ?>"><?php echo get_the_title( $id ); ?></a></h3>
+	</div>
     <div class="post-content">
-        <?php echo apply_filters('the_content',get_the_content()) ?>
-        <?php if(isset($mark->autor) && $mark->autor){ ?> <p class="marker-info"><?php echo __('Author', 'soundmap') . ': ' . $mark->autor; ?></br><?php } ?>
+        <?php echo apply_filters( 'the_content', get_the_content() ); ?>
+        <?php if( isset( $mark->autor ) && $mark->autor ) { ?>
+			<p class="marker-info">
+				<?php echo __('Author', 'soundmap') . ': ' . $mark->autor; ?></br>
+			</p>
+		<?php } ?>
         <hr class="clear">
         <audio class="soundmap-audio-player not-processed" src="<?php echo $mark->files[0]['url'] ?>"></audio>
         <div class="marker-info">
-        <?php the_tags(__('Tags','soundmap') . ': ', ' | ', '</br>'); ?>
-        <?php echo __('Categories','soundmap') . ': '; the_category(' | '); ?>
+			<?php the_tags( __( 'Tags', 'soundmap' ) . ': ', ' | ', '</br>' ); ?>
+			<?php echo __( 'Categories', 'soundmap' ) . ': '; the_category( ' | ' ); ?>
         </div>
     </div>
-
-
-</div>
+</div><!-- end .marker-window -->
